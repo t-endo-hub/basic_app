ba3355ae2fa33c1f9a2d9efc4d0c2a2b074847a4
Takumi Endo <endotaku0608@gmail.com>
2021-01-19 05:40:41 +0000

    [Add]エラーメッセージ個別化

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,22 @@ module StartApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.default_locale = :ja # 追記
+
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      if instance.kind_of?(ActionView::Helpers::Tags::Label)
+        html_tag.html_safe
+      else
+        class_name = instance.object.class.name.underscore
+        method_name = instance.instance_variable_get(:@method_name)
+        "<div class=\"has-error\">#{html_tag}
+          <span class=\"help-block\">
+            #{I18n.t("activerecord.attributes.#{class_name}.#{method_name}")}
+            #{instance.error_message.first}
+          </span>
+        </div>".html_safe
+      end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
